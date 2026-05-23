b27818e4a2105e930ecc48164b43284263b230f4
Mayank <mayank.kr0311@gmail.com>
2026-05-23 17:15:22 +0530

    add more interactive

--- server/server.js
+++ server/server.js
@@ -55,8 +55,8 @@ const corsOptions = {
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-Forwarded-For', 'X-Forwarded-Proto', 'CF-Connecting-IP'],
-  exposedHeaders: ['X-Total-Count', 'X-Current-Page']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-Forwarded-For', 'X-Forwarded-Proto', 'CF-Connecting-IP', 'Cache-Control', 'Pragma', 'Expires', 'Surrogate-Control'],
+  exposedHeaders: ['X-Total-Count', 'X-Current-Page', 'Content-Disposition']
 };
 
 app.use(cors(corsOptions));
