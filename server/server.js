72fc2bb1f96316c8c39d3baad404cb2cf268ed5c
Mayank <mayank.kr0311@gmail.com>
2026-05-27 23:26:44 +0530

    fix trust proxy

--- server/server.js
+++ server/server.js
@@ -63,8 +63,7 @@ app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
 // Trust Cloudflare proxy - CRITICAL for SSL
-const cloudflareIPs = (process.env.CLOUDFLARE_IPS || '103.21.244.0/22,103.22.200.0/22,103.31.4.0/22,104.16.0.0/12,108.162.192.0/18,131.0.72.0/22,141.101.64.0/18,162.158.0.0/15,172.64.0.0/13,173.245.48.0/20,188.114.96.0/20,190.93.240.0/20,197.234.240.0/22,198.41.128.0/17').split(',');
-app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal', '127.0.0.1', '::1', ...cloudflareIPs]);
+app.set('trust proxy', 1);
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
